6ae97bfbdf64e7e2ac9cfabad4a3229c4e24e0da
Rodrigo Amaro <jbravo.br@gmail.com>
2016-10-07 01:55:59 -0300

    Primeiro commit

---
+++ ListViewSeparatorLinesExample/ListViewSeparatorLinesExamplePage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using Xamarin.Forms;
+
+namespace ListViewSeparatorLinesExample
+{
+    public partial class ListViewSeparatorLinesExamplePage : ContentPage
+    {
+        public ListViewSeparatorLinesExamplePage()
+        {
+            InitializeComponent();
+            FillListView();
+        }
+
+        public void FillListView()
+        {
+            var listOfRandom = new List<Random>();
+            for (int i = 0; i < 2; i++)
+            {
+                listOfRandom.Add(new Random
+                {
+                    Name = System.IO.Path.GetRandomFileName(),
+                    Detail = System.IO.Path.GetRandomFileName()
+                });
+            }
+
+            var grouped = from r in listOfRandom
+                          orderby r.Name
+                          group r by r.NameSort into randomGrouped
+                          select new Grouping<string, Random>(randomGrouped.Key, randomGrouped);
+
+            var source = new ObservableCollection<Grouping<string,Random>>(grouped);
+            lvRandom.ItemsSource = source;
+        }
+    }
+
+    public class Random
+    {
+        public string Name { get; set; }
+        public string Detail { get; set; }
+
+        public string NameSort => Name[0].ToString();
+    }
+
+    public class Grouping<K, T> : ObservableCollection<T>
+    {
+        public K Key { get; private set; }
+
+        public Grouping(K key, IEnumerable<T> items)
+        {
+            Key = key;
+            foreach (var item in items)
+                Items.Add(item);
+        }
+    }
+}
---
+++ iOS/Renderes/ListViewRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CoreGraphics;
+using ListViewSeparatorLinesExample;
+using ListViewSeparatorLinesExample.iOS;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:Xamarin.Forms.ExportRenderer(typeof(CustomListViewSeparatorRenderer),typeof(ListViewRenderer_iOS))]
+namespace ListViewSeparatorLinesExample.iOS
+{
+    public class ListViewRenderer_iOS : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+            }
+
+            if (e.NewElement != null)
+            {
+                var EmptyView = new UIView(new CGRect()) { BackgroundColor = UIColor.Clear };
+                Control.TableFooterView = EmptyView;
+            }
+        }
+    }
+}
